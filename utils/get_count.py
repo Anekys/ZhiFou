5a0ae215d2026cccb23032d6af22167899347ece
zeronee@qq.com <271448116@qq.com>
2022-09-08 21:50:56 +0800

    为主页,搜索界面,问题详情界面增加了分页功能

---
+++ utils/get_count.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/9/8 14:22
+# @Author  : Ane
+# @email   : upapqqxyz@gmail.com
+# @FileName: get_count.py
+# @Software: PyCharm
---
+++ utils/get_word_count.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/9/8 21:39
+# @Author  : Ane
+# @email   : upapqqxyz@gmail.com
+# @FileName: get_word_count.py
+# @Software: PyCharm
